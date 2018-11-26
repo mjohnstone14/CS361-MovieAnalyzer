d0d40ad5c325d0454388c4e24ad771e77eba091e
Ben Shapiro <bshapiro@pugetsound.edu>
2018-11-26 02:04:37 -0800

    finished fw

--- src/graph/GraphAlgorithms.java
+++ src/graph/GraphAlgorithms.java
@@ -1,4 +1,5 @@
 package graph;
+import java.util.List;
 
 
 public class GraphAlgorithms {
@@ -6,7 +7,38 @@ public class GraphAlgorithms {
 
   }
   public static int[][] floydWarshall(Graph<Integer> graph){
-    
-  }
+    int vertNum = graph.numVertices();
+        int[][] distance = new int[vertNum+1][vertNum+1];
+
+        //make matrix
+        for(int search=1; search<=vertNum; search++){
+            for(int fin=1; fin<=vertNum; fin++){ //get start node neighbors
+                List<Integer> neighbors = graph.getNeighbors(search);
+                //start node is connected to finish node
+                if(neighbors.contains(fin)){
+                    distance[search][fin] = 1;
+                }
+                else{
+                    distance[search][fin] = Integer.MAX_VALUE/2; //node not connected to finish
+                }
+            }
+        }
+
+        //get shortest path for all
+        for(int mid =1; mid<=vertNum; mid++){
+            for(int search=1; search<=vertNum; search++){
+                for(int fin=1; fin<=vertNum; fin++){
+                    //If most efficient
+                    if(distance[search][mid] + distance[mid][fin] < distance[search][fin]){
+                        distance[search][fin] = distance[search][mid] + distance[mid][fin]; //updates matrix
+                    }
+                }
+            }
+        }
+        return distance;
+    }
+
+
+
 	// FILL IN
 }
