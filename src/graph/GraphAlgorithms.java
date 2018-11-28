378bf6d4c512cedf4511c7c6a6a6d683ba4130d6
Ben Shapiro <bshapiro@pugetsound.edu>
2018-11-27 18:01:58 -0800

    test graph

--- src/graph/GraphAlgorithms.java
+++ src/graph/GraphAlgorithms.java
@@ -70,7 +70,30 @@ public class GraphAlgorithms {
         return distance;
     }
 
+    public static void main (String [] args){
+        Graph <Integer> g = new Graph<>();
+        g.addVertex(1);
+        g.addVertex(2);
+        g.addVertex(3);
+        g.addVertex(4);
+        g.addVertex(5);
+        g.addEdge(1,2);
+        g.addEdge(2,3);
+        g.addEdge(3,4);
+        g.addEdge(4,5);
+        g.addEdge(1,3);
+        g.addEdge(1,4);
+        g.addEdge(3,2);
+        g.addEdge(2,4);
+        int[][] fw = floydWarshall(g);
+        System.out.println(fw);
+
+    }
 
 
-	// FILL IN
+
+
+
+
+    // FILL IN
 }
